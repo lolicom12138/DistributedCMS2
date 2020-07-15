83a19249f829f67fbe11738132fe0c9aa82558ce
liyuan <liyuan890214@live.fr>
2020-07-15 16:42:15 +0800

    Initial commit

---
+++ forum-service/src/test/java/xyz/apichiyu/distributedcms/forumservice/ForumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.apichiyu.distributedcms.forumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
